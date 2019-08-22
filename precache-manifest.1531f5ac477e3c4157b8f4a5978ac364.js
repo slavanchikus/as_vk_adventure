edfa851c4c5d0653241011adcb57127503961ae5
root <root@DESKTOP-I2FSH0P.localdomain>
2019-08-22 16:55:01 +0300

    Updates

---
+++ precache-manifest.1531f5ac477e3c4157b8f4a5978ac364.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bbd2a2cb3a7e1c57321de9c96353d76",
+    "url": "/slavanchikus/as_vk_adventure/static/media/start.5bbd2a2c.svg"
+  },
+  {
+    "revision": "b6d2069a918a8118159aeddda0b5b87c",
+    "url": "/slavanchikus/as_vk_adventure/static/media/logo.b6d2069a.svg"
+  },
+  {
+    "revision": "f1341cf8a5d47ed955d3f4f6a2fb0d8a",
+    "url": "/slavanchikus/as_vk_adventure/static/media/circle.f1341cf8.svg"
+  },
+  {
+    "revision": "d29faa70f82803ec28d14e3445ca94b5",
+    "url": "/slavanchikus/as_vk_adventure/static/media/background.d29faa70.svg"
+  },
+  {
+    "revision": "bad859f465888e1a2750",
+    "url": "/slavanchikus/as_vk_adventure/static/js/runtime~main.44d18a60.js"
+  },
+  {
+    "revision": "3e8c061a9449311f57da",
+    "url": "/slavanchikus/as_vk_adventure/static/js/main.fe36cc9b.chunk.js"
+  },
+  {
+    "revision": "df57894bd24e5d23bc13",
+    "url": "/slavanchikus/as_vk_adventure/static/js/2.65594127.chunk.js"
+  },
+  {
+    "revision": "3e8c061a9449311f57da",
+    "url": "/slavanchikus/as_vk_adventure/static/css/main.b7798b0a.chunk.css"
+  },
+  {
+    "revision": "c787ffc05ab3c95380ac0b1f9822eb17",
+    "url": "/slavanchikus/as_vk_adventure/index.html"
+  }
+];

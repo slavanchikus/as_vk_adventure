07743a062dfc90e3dcddecaaab86fca488dbbb17
root <root@DESKTOP-I2FSH0P.localdomain>
2019-08-26 10:58:42 +0300

    Updates

--- precache-manifest.6ebc86613cd5e277fb7fea5acf21669c.js
+++ precache-manifest.6ebc86613cd5e277fb7fea5acf21669c.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/as_vk_adventure/static/media/paris.9abae974.png"
   },
   {
-    "revision": "8a65c4c913137a75ad14",
+    "revision": "0ad92c9662520fbe166c",
     "url": "/as_vk_adventure/static/css/main.bc843893.chunk.css"
   },
   {
@@ -16,20 +16,16 @@ self.__precacheManifest = [
     "url": "/as_vk_adventure/static/js/2.8ea21ab5.chunk.js"
   },
   {
+    "revision": "f1341cf8a5d47ed955d3f4f6a2fb0d8a",
+    "url": "/as_vk_adventure/static/media/circle.f1341cf8.svg"
+  },
+  {
     "revision": "5bbd2a2cb3a7e1c57321de9c96353d76",
     "url": "/as_vk_adventure/static/media/start.5bbd2a2c.svg"
   },
   {
     "revision": "a8d9e010d5a4dac9647002cf9dc6e861",
     "url": "/as_vk_adventure/static/media/share.a8d9e010.svg"
-  },
-  {
-    "revision": "f1341cf8a5d47ed955d3f4f6a2fb0d8a",
-    "url": "/as_vk_adventure/static/media/circle.f1341cf8.svg"
-  },
-  {
-    "revision": "d29faa70f82803ec28d14e3445ca94b5",
-    "url": "/as_vk_adventure/static/media/background.d29faa70.svg"
   },
   {
     "revision": "b6d2069a918a8118159aeddda0b5b87c",
@@ -40,16 +36,20 @@ self.__precacheManifest = [
     "url": "/as_vk_adventure/static/media/join.40578453.svg"
   },
   {
-    "revision": "00749852179253b055747c4145949035",
-    "url": "/as_vk_adventure/static/media/london.00749852.png"
+    "revision": "d29faa70f82803ec28d14e3445ca94b5",
+    "url": "/as_vk_adventure/static/media/background.d29faa70.svg"
   },
   {
     "revision": "868877efc61841c0415362525bf78c99",
     "url": "/as_vk_adventure/static/media/saintp.868877ef.png"
   },
   {
-    "revision": "8a65c4c913137a75ad14",
-    "url": "/as_vk_adventure/static/js/main.8d0eebdd.chunk.js"
+    "revision": "00749852179253b055747c4145949035",
+    "url": "/as_vk_adventure/static/media/london.00749852.png"
+  },
+  {
+    "revision": "0ad92c9662520fbe166c",
+    "url": "/as_vk_adventure/static/js/main.b8e4d74d.chunk.js"
   },
   {
     "revision": "0ab28359924edbc30fd26ba76a43fcef",
@@ -60,12 +60,12 @@ self.__precacheManifest = [
     "url": "/as_vk_adventure/static/media/pisa.57f0f5ac.png"
   },
   {
-    "revision": "6c63ab02347ca5c89bd3173389345559",
-    "url": "/as_vk_adventure/static/media/kaluga.6c63ab02.png"
-  },
-  {
     "revision": "17e83ea45409d6861276ca8bc198dde3",
     "url": "/as_vk_adventure/static/media/amsterdam.17e83ea4.png"
+  },
+  {
+    "revision": "6c63ab02347ca5c89bd3173389345559",
+    "url": "/as_vk_adventure/static/media/kaluga.6c63ab02.png"
   },
   {
     "revision": "f53c8190d0da375f1d7155b567ad7452",
@@ -92,7 +92,7 @@ self.__precacheManifest = [
     "url": "/as_vk_adventure/static/media/everest.e8a54afe.png"
   },
   {
-    "revision": "f5f4ba13d9a6ffd26fc45ffdcd7ec05d",
+    "revision": "c1a760a131e9fdcd6e6a345859f847ab",
     "url": "/as_vk_adventure/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/as_vk_adventure/precache-manifest.7e0e0b16d0238ebdea0f8af1c1c896ec.js"
+  "/as_vk_adventure/precache-manifest.6ebc86613cd5e277fb7fea5acf21669c.js"
 );
 
 workbox.clientsClaim();
